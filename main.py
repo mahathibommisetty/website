25efd3dcd8be86509ed6967de51c911c7df66130
mahathi <mahathishetty6@gmail.com>
2023-11-29 15:03:55 +0530

    isetj

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import streamlit as st
 st.set_page_config(layout='wide')
-st.title('My title')
-st.write("hello")
-st.code('for i in range(8): foo()')
-st.toggle("Enable")
-st.camera_input("enter the camera")
-st.snow()
-st.balloons()
+st.title('c programming')
+st.write("factorial")
+st.code("""
+#include <stdio.h>
+int factorial(int n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+int main() {
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    int result = factorial(number);
+    printf("The factorial of %d is %d\n", number, result);
+    return 0;
+}
+""")
