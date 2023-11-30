2856bf9582d6beb49e39ccf3956167169be52bab
mahathi <mahathishetty6@gmail.com>
2023-11-30 12:15:53 +0530

    c programs

--- pages/pointers.py
+++ pages/pointers.py
@@ -78,3 +78,23 @@ int main() {
     return 0;
 }
 """)
+
+
+st.subheader("swapping of 2 numbers using functions")
+st.code("""
+#include <stdio.h>
+void swap(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+int main() {
+  int num1, num2;
+  printf("enter two numbers num1, num2 :  ");
+  scanf("%d %d",&num1,&num2);
+  printf("Before swapping: num1 = %d, num2 = %d\n", num1, num2);
+  swap(&num1, &num2);
+  printf("After swapping: num1 = %d, num2 = %d\n", num1, num2);
+  return 0;
+}
+""")
