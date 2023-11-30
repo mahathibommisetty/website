0fdb6a8c1794c3d90a3401900af1c78b91cb21ca
mahathi <mahathishetty6@gmail.com>
2023-11-30 21:18:44 +0530

    c programs

--- pages/control statements.py
+++ pages/control statements.py
@@ -3,48 +3,6 @@ st.set_page_config(layout='wide')
 st.title("c programming")
 st.header("control statements")
 tab1, tab2, tab3, tab4, tab5 = st.tabs(["if statement", "if-else statement", "for-loop statement", "while-loop statement", "switch statement"])
-with tab5:
-  st.subheader("switch statement")
-  st.code("""
-  #include<stdio.h>
-  int main(){
-    int num1,num2;
-    char operator;
-    printf("enter a number1");
-    scanf("%d",&num1);
-    printf("enter number2");
-    scanf("%d",&num2);
-    switch(operator){
-    case'+':printf("addition is %d",(num2+num1));
-    break;
-    case'-':printf("subtraction is %d",(num2-num));
-    break;
-    case'*':printf("multiplication is %d",(num2*num1));
-    break;
-    case'/':printf("division is %d",(num2/num1));
-    break;
-    default:printf("not exist");
-}
-}
-""")
-
-
-with tab4:
-    st.subheader(" while statement")
-    st.code("""
-    #include<stdio.h>
-    int main(){
-        int num=1,stop;
-            while(1){
-            printf("enter the key:");
-            scanf("%d",&stop);
-            if(stop==0){
-            break;
-      }
-     }
-    }
-    """)
-
 
 with tab2:
     st.subheader(" if-else statement")
@@ -68,16 +26,16 @@ with tab3:
     tab6, tab7, tab8 =(["tree 1", "tree 2", "tree 3"])
     with tab6:
        st.subheader("tree 1")
-      st.code("""
-      #include<stdio.h>
-      int main()
-      for(int i=0; i<3;i++){
-      for(int j=0;j<=i;j++){
-      printf("*");
-      }
-      printf("/n")
-      }
-    }
+       st.code("""
+          #include<stdio.h>
+          int main()
+          for(int i=0; i<3;i++){
+          for(int j=0;j<=i;j++){
+          printf("*");
+          }
+          printf("/n")
+          }
+        }
     """)
 
 
@@ -109,3 +67,51 @@ with tab3:
         }
         }
     """)
+
+
+with tab4:
+    st.subheader(" while statement")
+    st.code("""
+    #include<stdio.h>
+    int main(){
+        int num=1,stop;
+            while(1){
+            printf("enter the key:");
+            scanf("%d",&stop);
+            if(stop==0){
+            break;
+      }
+     }
+    }
+    """)
+
+with tab5:
+  st.subheader("switch statement")
+  st.code("""
+  #include<stdio.h>
+  int main(){
+    int num1,num2;
+    char operator;
+    printf("enter a number1");
+    scanf("%d",&num1);
+    printf("enter number2");
+    scanf("%d",&num2);
+    switch(operator){
+    case'+':printf("addition is %d",(num2+num1));
+    break;
+    case'-':printf("subtraction is %d",(num2-num));
+    break;
+    case'*':printf("multiplication is %d",(num2*num1));
+    break;
+    case'/':printf("division is %d",(num2/num1));
+    break;
+    default:printf("not exist");
+}
+}
+""")
+
+
+
+
+
+
